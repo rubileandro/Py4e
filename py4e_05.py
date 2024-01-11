43beaedbbd7529fe93d5249cc0e337c439c4d981
Rubi Leandro <rubi@brislandventures.com>
2024-01-11 18:52:01 +0400

    Moved the previous code it's it's correct file. Program takes in a score from the user and outputs a result, if the score is in a valid range.

--- py4e_05.py
+++ py4e_05.py
@@ -1,23 +1,33 @@
 """
-Write a program to prompt the user for hours and rate per hour using input to compute gross pay.
-Pay should be the normal rate for hours up to 40 and time-and-a-half for the hourly rate for all hours worked above 40 hours. 
-Put the logic to do the computation of pay in a function called computepay() and use the function to do the computation. 
-The function should return a value. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75).
-You should use input to read a string and float() to convert the string to a number. 
-Do not worry about error checking the user input unless you want to - you can assume the user types numbers properly. 
-Do not name your variable sum or use the sum() function.
+3.3 Write a program to prompt for a score between 0.0 and 1.0. 
+If the score is out of range, print an error. 
+If the score is between 0.0 and 1.0, print a grade using the following table:
+
+Score Grade
+>= 0.9 A
+>= 0.8 B
+>= 0.7 C
+>= 0.6 D
+< 0.6 F
+
+If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
 """
 
-hours = float(input("Please enter your hours worked: "))
-rate = float(input("Please enter your rate: "))
+score = float(input("Please enter a score between 0.0 and 1.0: "))
+table = "Score Grade \n >= 0.9 A\n >= 0.8 B\n >= 0.7 C\n >= 0.6 D\n < 0.6 F\n"
 
-def computepay(hours,rate):
-    if hours <= 40:
-        pay = hours * rate
-        print(pay)
+if score >= 0.0 and score <= 1.0:
+    # print(table)
+    if score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
     else:
-        pay = (40 * rate) + ((hours - 40) * (rate * 1.5))
-        return  pay
+        print("F")
+else: 
+    print("You entered an invalid score. Please read the instruction again and enter a valid score.")
     
-pay = computepay(hours, rate)
-print(f"Pay {pay}")
